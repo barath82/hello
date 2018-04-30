53161a33b0df56abc41ab330fdd038420168cab6
Barath <barath@meemmemory.com>
2018-04-30 18:00:04 +0530

    hello

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-var http = require('http');
+//var http = require('http');
+//
+//var server = http.createServer(function(request, response) {
+//
+//    response.writeHead(200, {"Content-Type": "text/plain"});
+//    response.end("Hello World!");
+//
+//});
+//
+//var port = process.env.PORT || 1337;
+//server.listen(port);
+//
+//console.log("Server running at http://localhost:%d", port);
+var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
+var key = fs.readFileSync('./certs/ssl/Server.key');
+var cert = fs.readFileSync('./certs/ssl/Server.crt');
+var ca = fs.readFileSync('./certs/ssl/CA.crt');
 
+var options = {
+    key: key,
+    cert: cert,
+    ca: ca
+};
+
+var https = require('https');
+
+var server = https.createServer(options, function(request, response) {
+    console.log(request.url);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
@@ -10,4 +35,4 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at https://localhost:%d", port);
